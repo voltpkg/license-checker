832b0ae2628e43a187241456d5ebe5e5c3649b59
Andrew <andrewnijmeh1@gmail.com>
2021-11-27 15:31:18 -0500

    create trait for resolv ext

--- src/query.rs
+++ src/query.rs
@@ -15,3 +15,15 @@ impl PackagesExt for Vec<Package> {
             .ok_or_else(|| anyhow!("Couldn't find the package {}", id))           
     }
 }
+
+pub trait ResolveExt {
+    fn by_id(&self, id: &Packageid) -> anyhow::Result<&[NodeDep]> {
+        self.nodes
+            .iter()
+            .find(|node| &node.id == id)
+            .map(|node| node.deps.as_ref())
+            .ok_or_else(|| anyhow!("Couldn't find deps for package {}", id))
+    }
+}
+
+
