64d02f605e29d29d4bf1e63a6fb5c527aa39364f
Andrew <andrewnijmeh1@gmail.com>
2021-12-06 18:38:07 -0500

    create impl for By

--- src/options.rs
+++ src/options.rs
@@ -5,14 +5,67 @@ use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 pub type PackageIdSpec = String;
 
 #[derive(Copy, Clone, Debug)]
-pub enum by {
+pub enum By {
     License,
     Package,
 }
 
-#[dervice(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum SelectedPackage {
     All,
     Default,
     Specific(PackageIdSpec),
+}
+
+#[derive(Clone, Debug)]
+pub enum Bundle {
+    Inline { file: Option<String> },
+    NameOnly { file: Option<String> },
+    Source { file: Option<String> },
+    Split { file: Option<String>, dir: String },
+}
+
+#[derive(Clone, Debug)]
+#[allow(clippy::large_enum_variant)]
+pub enum Cmd {
+    List {
+        by: By,
+        package: SelectedPackage,
+    },
+    Check {
+        package: SelectedPackage,
+    },
+    Bundle {
+        variant: Bundle,
+        package: SelectedPackage,
+    },
+    ThirdParty {
+        full: bool,
+    },
+}
+
+#[derive(Clone, Debug)]
+pub struct Options {
+    pub verbose: u32,
+    pub quiet: bool,
+    pub color: Option<String>
+}
+
+impl By {
+    fn args() -> Vec<arg<'static, 'static>> {
+        vec![Arg::with_name("by")
+            .long("by")
+            .takes_value(true)
+            .possible_values(&['license', 'crate'])
+            .default_value("license")
+            .help("List package per license or license per pacakge.")]
+    }
+
+    fn from_matches(matches: &ArgMatches) -> By {
+        matches
+            .value_of("by")
+            .expect("defaulted")
+            .parse()
+            .expect("constrained")
+    }
 }
